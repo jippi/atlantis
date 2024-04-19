74e7f690feec1c17f27592e7b22d0ecbf5bf9f82
Christian Winther <christian@seatgeek.com>
2024-04-19 18:41:59 +0200

    fix: hmm

--- server/utils/os.go
+++ server/utils/os.go
@@ -3,6 +3,9 @@ package utils
 import "os"
 
 // RemoveIgnoreNonExistent removes a file, ignoring if it doesn't exist.
+//
+// There are many cases where we just want to make sure a file is gone, and
+// don't really care if it never existed to begin with.
 func RemoveIgnoreNonExistent(file string) error {
 	err := os.Remove(file)
 	if err == nil || os.IsNotExist(err) {
